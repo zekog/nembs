e87922b0a9e6c1bd844ac48de286be963076c4b7
zekog <80773353+zekog@users.noreply.github.com>
2024-04-05 23:36:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const targetDate = new Date(2024, 3, 7, 11, 30, 00); // Ustaw datę premiery
 const countdownContainer = document.getElementById("countdown-container");
 const outNowText = document.getElementById("out-now-text");
+const coverImage = document.getElementById("cover-image");
+const linkContainer = document.getElementById("links-container");
+const Title = document.getElementById("title");
+const mainContainer = document.getElementById("main-container");
+
+    // Dodanie animacji FadeIn z diagnostyką
+    console.log("Dodawanie klas 'fadeIn'..."); // Diagnostyka
+    countdownContainer.classList.add("fadeIn");
+    outNowText.classList.add("fadeIn");
+    coverImage.classList.add("fadeIn");
+    linkContainer.classList.add("fadeIn");
+    Title.classList.add("fadeIn");
 
 const updateCountdown = () => {
   const currentDate = new Date();
@@ -17,11 +29,19 @@ const updateCountdown = () => {
   document.getElementById("seconds-left").textContent = secondsLeft;
 
   // Sprawdź, czy odliczanie zakończyło się
-  if (diff <= 0) {
+    if (diff <= 0) {
     countdownContainer.style.display = "none";
     outNowText.style.display = "block";
+
+    // Dodanie animacji FadeIn z diagnostyką
+    console.log("Dodawanie klas 'fadeIn'..."); // Diagnostyka
+    countdownContainer.classList.add("fadeIn");
+    outNowText.classList.add("fadeIn");
+    coverImage.classList.add("fadeIn");
+    linkContainer.classList.add("fadeIn");
+    Title.classList.add("fadeIn");
   }
 };
 
 setInterval(updateCountdown, 1000);
-updateCountdown(); // Uruchom odliczanie
+updateCountdown(); 
